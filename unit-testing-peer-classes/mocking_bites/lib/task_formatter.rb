7382582408e13c1f882d1eed5692b4043ca156b4
henrychow <113641610+henrow5@users.noreply.github.com>
2022-11-16 22:40:42 +0000

    Small refactor

--- unit-testing-peer-classes/mocking_bites/lib/task_formatter.rb
+++ unit-testing-peer-classes/mocking_bites/lib/task_formatter.rb
@@ -1,14 +1,20 @@
-class TaskFormatter
-  def initialize(task) # task is an instance of Task
-    @task = task
-  end
-
+class TaskFormatter < Struct.new(:task)
   def format
-    # Returns the task formatted as a string.
-    # If the task is not complete, the format is:
-    # - [ ] Task title
-    # If the task is complete, the format is:
-    # - [x] Task title
     return "- [#{@task.complete? ? "x" : " "}] #{@task.title}"
   end
 end
+
+# class TaskFormatter
+#   def initialize(task) # task is an instance of Task
+#     @task = task
+#   end
+
+#   def format
+#     # Returns the task formatted as a string.
+#     # If the task is not complete, the format is:
+#     # - [ ] Task title
+#     # If the task is complete, the format is:
+#     # - [x] Task title
+#     return "- [#{@task.complete? ? "x" : " "}] #{@task.title}"
+#   end
+# end
